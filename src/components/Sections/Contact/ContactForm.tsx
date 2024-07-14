5bf4a8e2c96926b1e3a30043ed08a5573eeb9ca8
John Pan <john.pan@mail.mcgill.ca>
2024-07-13 23:52:24 -0400

    feat: Formatting corrections

--- src/components/Sections/Contact/ContactForm.tsx
+++ src/components/Sections/Contact/ContactForm.tsx
@@ -1,4 +1,5 @@
 import {FC, memo, useCallback, useMemo, useState} from 'react';
+import { useForm, ValidationError } from '@formspree/react';
 
 interface FormData {
   name: string;
@@ -40,34 +41,64 @@ const ContactForm: FC = memo(() => {
     [data],
   );
 
+  
   const inputClasses =
     'bg-neutral-700 border-0 focus:border-0 focus:outline-none focus:ring-1 focus:ring-orange-600 rounded-md placeholder:text-neutral-400 placeholder:text-sm text-neutral-200 text-sm';
 
+  const [state, handleSubmit] = useForm("xblrljnl");
+    if (state.succeeded) {
+        return <p>Thanks for contacting me, I will get back to you ASAP!</p>;
+    }
+
+  
   return (
-    <form className="grid min-h-[320px] grid-cols-1 gap-y-4" method="POST" onSubmit={handleSendMessage}>
-      <input className={inputClasses} name="name" onChange={onChange} placeholder="Name" required type="text" />
+    <form onSubmit={handleSubmit} className="grid min-h-[320px] grid-cols-1 gap-y-4">
+      <label htmlFor="name">
+      Email Address
+    </label>
+      <input id="name" className={inputClasses} name="name" placeholder="Name" type="text" />
+      <ValidationError 
+        prefix="Name" 
+        field="name"
+        errors={state.errors}
+      />
+      <label htmlFor="email">
+      Email Address
+    </label>
       <input
+        id="email"
         autoComplete="email"
         className={inputClasses}
         name="email"
-        onChange={onChange}
         placeholder="Email"
-        required
         type="email"
       />
+      <ValidationError 
+        prefix="Email" 
+        field="email"
+        errors={state.errors}
+      />
+      <label htmlFor="message">
+      Email Address
+    </label>
       <textarea
+        id="message"
         className={inputClasses}
         maxLength={250}
         name="message"
-        onChange={onChange}
         placeholder="Message"
-        required
         rows={6}
+      />
+      <ValidationError 
+        prefix="Message" 
+        field="message"
+        errors={state.errors}
       />
       <button
         aria-label="Submit contact form"
         className="w-max rounded-full border-2 border-orange-600 bg-stone-900 px-4 py-2 text-sm font-medium text-white shadow-md outline-none hover:bg-stone-800 focus:ring-2 focus:ring-orange-600 focus:ring-offset-2 focus:ring-offset-stone-800"
-        type="submit">
+        type="submit"
+        disabled={state.submitting}>
         Send Message
       </button>
     </form>
--- src/data/data.tsx
+++ src/data/data.tsx
@@ -2,22 +2,22 @@ import {
   AcademicCapIcon,
   ArrowDownTrayIcon,
   BuildingOffice2Icon,
-  CalendarIcon,
+  // CalendarIcon,
   FlagIcon,
   MapIcon,
   SparklesIcon,
 } from '@heroicons/react/24/outline';
 
 import GithubIcon from '../components/Icon/GithubIcon';
-import InstagramIcon from '../components/Icon/InstagramIcon';
+// import InstagramIcon from '../components/Icon/InstagramIcon';
 import LinkedInIcon from '../components/Icon/LinkedInIcon';
-import StackOverflowIcon from '../components/Icon/StackOverflowIcon';
-import TwitterIcon from '../components/Icon/TwitterIcon';
+// import StackOverflowIcon from '../components/Icon/StackOverflowIcon';
+// import TwitterIcon from '../components/Icon/TwitterIcon';
 import heroImage from '../images/header-background.webp';
 import memoriesApp from '../images/portfolio/memoriesapp.jpg';
-import rasppiCar from '../images/portfolio/rpcar.jpg';
+import rasppiCar from '../images/portfolio/rpcar1.jpg';
 import carDashboardUi from '../images/portfolio/cardashboardui.jpg';
-import carServer from '../images/portfolio/carserver.jpg';
+import carServer from '../images/portfolio/carserver1.jpg';
 import talkingPlant from '../images/portfolio/talkingplant.jpg'
 import profilepic from '../images/profilepic.jpg';
 import testimonialImage from '../images/testimonial.webp';
@@ -38,8 +38,8 @@ import {
  * Page meta data
  */
 export const homePageMeta: HomepageMeta = {
-  title: 'React Resume Template',
-  description: "Example site built with Tim Baker's react resume template",
+  title: "John's hideout",
+  description: "My personal website =)",
 };
 
 /**
@@ -72,8 +72,8 @@ export const heroData: Hero = {
       </p>
       <p className="prose-sm text-stone-200 sm:prose-base lg:prose-lg">
         In my free time time, you can catch me on the <strong className="text-stone-100">pickle ball</strong> field,
-        bashing my <strong className="text-stone-100">piano</strong>, or fiddling with my 
-        <strong className="text-stone-100">Raspberry Pi</strong>.
+        bashing my <strong className="text-stone-100">piano</strong>, or fiddling with my  
+        <strong className="text-stone-100"> Raspberry Pi</strong>.
       </p>
     </>
   ),
@@ -97,11 +97,10 @@ export const heroData: Hero = {
  */
 export const aboutData: About = {
   profileImageSrc: profilepic,
-  description: `Hi! I'm a generalist Software Engineer with a blend of aerospace and tech expertise. 
+  description: `Hi! I'm an Engineer with a blend of aerospace and tech expertise. 
   My journey in aerospace has taught me precision and efficiency, which I now bring to the software world in every project I tackle. 
   With a Bachelor's in Mechanical Engineering from McGill University and ongoing Master's in Computer Science at Georgia Tech, 
   I bring a multidisciplinary approach to software development.
-  
   I love tackling complex challenges using the latest tools to build and deploy full-stack web applications, 
   working with technologies like Vue.js, Django REST Framework, Gitlab-Ci, Kubernetes, and Docker. I am still constantly learning new tools to integrate into my workflow.`,
   aboutItems: [
@@ -152,7 +151,7 @@ export const skills: SkillGroup[] = [
     skills: [
       {
         name: 'Django REST Framework',
-        level: 8,
+        level: 9,
       },
       {
         name: 'Node.js',
@@ -169,7 +168,7 @@ export const skills: SkillGroup[] = [
     skills: [
       {
         name: 'Gitlab-CI',
-        level: 8,
+        level: 9,
       },
       {
         name: 'Kubernetes',
@@ -182,6 +181,10 @@ export const skills: SkillGroup[] = [
       {
         name: 'Docker',
         level: 7,
+      },
+      {
+        name: 'OpenShift',
+        level: 7
       },
     ],
   },
@@ -205,7 +208,7 @@ export const portfolioItems: PortfolioItem[] = [
   },
   {
     title: 'Talking Plant',
-    description: 'Raspberry Pi project to give speech abilities to any plant, giving them the ability to ask for water, sunlight and help against predators (my cat).',
+    description: 'Project to bestow plants the power of speech, giving them the ability to ask for water, sunlight and help against predators (my cat).',
     url: 'https://github.com/jpan8866/talking-plant',
     image: talkingPlant,
   },
@@ -235,7 +238,7 @@ export const education: TimelineItem[] = [
   },
   {
     date: 'January 2024 - May 2024',
-    location: 'University of Illinois Urbana Champaign',
+    location: 'University of Illinois Urbana-Champaign',
     title: 'Masters in Computer Science',
     content: <p>CGPA: 4.0/4.0. Transfered to Georgia Tech.</p>,
   },
@@ -254,12 +257,12 @@ export const experience: TimelineItem[] = [
     location: 'Bell Canada',
     title: 'Software Engineer',
     content: (
-      <p>
-        ●	Led and developed the full-stack integration of 5 features, achieving significant process improvements, including an 80% reduction in lead time for cloud deployments, saving an average of 450 man-hours. <strong>(Vue2 Typescript, Python Django REST Framework)</strong>
-        ●	Implemented numerous UI features and components in a scaled trunk-based development environment, including user-friendly admin pages with dynamic filtering and table functionalities, which accelerated daily administrative tasks such as data management by 90%. <strong>(Vue2, Typescript)</strong>
-        ●	Built, and maintained robust CI/CD pipelines for new and existing applications, resulting in an 80% improvement in the deployment speed of new updates and 0% error rate with no rollback incidents since launching the new application into production. <strong>(OpenShift, Kubernetes, Docker, Gitlab-CI, Artifactory, Helm, Vault)</strong>
-        ●	Developed comprehensive Grafana dashboards to visualize backend data as well as application health monitoring enabling real-time analytics and insights. <strong>(Grafana, Prometheus)</strong>
-      </p>
+        <ul className="list-disc pl-5">
+          <li className="mb-1">Led and developed the full-stack integration of 5 features, achieving significant process improvements, including an 80% reduction in lead time for cloud deployments, saving an average of 450 man-hours. <strong>(Vue2 Typescript, Python Django REST Framework)</strong></li>
+        	<li className="mb-1">Implemented numerous UI features and components in a scaled trunk-based development environment, including user-friendly admin pages with dynamic filtering and table functionalities, which accelerated daily administrative tasks such as data management by 90%. <strong>(Vue2, Typescript)</strong></li>
+          <li className="mb-1">Built, and maintained robust CI/CD pipelines for new and existing applications, resulting in an 80% improvement in the deployment speed of new updates and 0% error rate with no rollback incidents since launching the new application into production. <strong>(OpenShift, Kubernetes, Docker, Gitlab-CI, Artifactory, Helm, Vault)</strong></li>
+          <li className="mb-1">Developed comprehensive Grafana dashboards to visualize backend data as well as application health monitoring enabling real-time analytics and insights. <strong>(Grafana, Prometheus)</strong></li>  
+        </ul>
     ),
   },
   {
@@ -267,9 +270,9 @@ export const experience: TimelineItem[] = [
     location: 'Damotech',
     title: 'Project Engineer',
     content: (
-      <p>
-        ●	Undertook the development of a machine learning model <strong>(CNN)</strong> to classify rack damage image data to assist inspection duties. <strong>(Scikit-learn)</strong>
-      </p>
+      <ul className="list-disc pl-5">
+        <li>Undertook the development of a machine learning model <strong>(CNN)</strong> to classify rack damage image data to assist inspection duties. <strong>(Scikit-learn)</strong></li>
+      </ul>
     ),
   },
 ];
@@ -277,26 +280,26 @@ export const experience: TimelineItem[] = [
 /**
  * Testimonial section
  */
-// export const testimonial: TestimonialSection = {
-//   imageSrc: testimonialImage,
-//   testimonials: [
-//     {
-//       name: 'John Doe',
-//       text: 'Use this as an opportunity to promote what it is like to work with you. High value testimonials include ones from current or past co-workers, managers, or from happy clients.',
-//       image: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/169.jpg',
-//     },
-//     {
-//       name: 'Jane Doe',
-//       text: 'Here you should write some nice things that someone has said about you. Encourage them to be specific and include important details (notes about a project you were on together, impressive quality produced, etc).',
-//       image: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/14.jpg',
-//     },
-//     {
-//       name: 'Someone else',
-//       text: 'Add several of these, and keep them as fresh as possible, but be sure to focus on quality testimonials with strong highlights of your skills/work ethic.',
-//       image: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/69.jpg',
-//     },
-//   ],
-// };
+export const testimonial: TestimonialSection = {
+  imageSrc: testimonialImage,
+  testimonials: [
+    // {
+    //   name: 'John Doe',
+    //   text: 'Use this as an opportunity to promote what it is like to work with you. High value testimonials include ones from current or past co-workers, managers, or from happy clients.',
+    //   image: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/169.jpg',
+    // },
+    // {
+    //   name: 'Jane Doe',
+    //   text: 'Here you should write some nice things that someone has said about you. Encourage them to be specific and include important details (notes about a project you were on together, impressive quality produced, etc).',
+    //   image: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/14.jpg',
+    // },
+    // {
+    //   name: 'Someone else',
+    //   text: 'Add several of these, and keep them as fresh as possible, but be sure to focus on quality testimonials with strong highlights of your skills/work ethic.',
+    //   image: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/69.jpg',
+    // },
+  ],
+};
 
 /**
  * Contact section
@@ -304,17 +307,16 @@ export const experience: TimelineItem[] = [
 
 export const contact: ContactSection = {
   headerText: 'Get in touch.',
-  description: 'Here is a good spot for a message to your readers to let them know how best to reach out to you.',
+  description: 'Eager to connect with you!',
   items: [
     {
       type: ContactType.Email,
       text: 'john.pan@mail.mcgill.ca',
-      href: 'mailto:john.pan@mail.mcgill.ca',
     },
     {
       type: ContactType.Location,
       text: 'New York, USA',
-      href: 'https://www.google.ca/maps/place/Victoria,+BC/@48.4262362,-123.376775,14z',
+      href: 'https://maps.app.goo.gl/MjHPaA1SNxJQTrdM9',
     },
     {
       type: ContactType.LinkedIn,
